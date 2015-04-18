90c7f81a5dbc25f33955bf2093dd3bc7bb7150dd
Michael Romano <michaelfromano@gmail.com>
2015-04-18 15:46:26 -0400

    asdfkjs

--- neds_statistics.py
+++ neds_statistics.py
@@ -22,7 +22,7 @@ URETHRAL_INJURY_CODES = ('8670','8671')
 '''
 ''' Descriptive statistics
     Average length of stay for admitted patients with erectile fracture: 1.38 days
-    
+
 '''
 
 '''
@@ -377,8 +377,8 @@ def average_los(filename):
     return float(los_total)/float(num_patients)
 
 def main():
-    print(total_ed_event('cleaned_data/core_patients_cleaned.csv'),2)
-    print(total_ed_event('cleaned_data/core_patients_cleaned.csv'),3)
+    print(total_ed_event('cleaned_data/core_patients_cleaned.csv',2)
+    print(total_ed_event('cleaned_data/core_patients_cleaned.csv',3)
     print(average_los('cleaned_data/ip_patients_cleaned.csv'))
     # print(average_charges_ip('cleaned_data/ip_patients_cleaned.csv'))
 
