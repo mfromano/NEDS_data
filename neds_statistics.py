dbe5ea375178b25f3201890a7d836414c851fe31
Michael Romano <michaelfromano@gmail.com>
2015-04-21 20:06:15 -0400

    sdfk

--- neds_statistics.py
+++ neds_statistics.py
@@ -448,7 +448,7 @@ def test_erectile_fracture_code():
 def main():
     choices = [1, 2, 3, 9, 98, 99]
     for choice in choices:
-        print('Total ed event: {0}'.format(str(total_ed_event('patient',choice)),))
+        print('Total ed event: {0}'.format(str(total_ed_event('cleaned_data/core_patients_cleaned.csv',choice)),))
 
     print(test_erectile_fracture_code())
     print('Average age of control group: {0}'.format(str(average_age('cleaned_data/core_controls_cleaned.csv')),))
