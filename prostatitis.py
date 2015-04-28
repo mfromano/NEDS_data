20441e8eb89acf0f20930a3bea6c1ea2239dca4e
Michael Romano <michaelfromano@gmail.com>
2015-04-28 17:36:58 -0400

    asdf

--- prostatitis.py
+++ prostatitis.py
@@ -3,7 +3,7 @@ import csv
 # 10415 = total cases in 2012 without chronic prostatitis
 # 10418 = total cases in 2012 (including chronic prostatitis)
 
-TORSION_CODE = '60820'
+TORSION_CODES = ['60820','60821','60822','60823','60824']
 PROSTATITIS_CODE = '6019'
 CHRONIC_CODE = '6011'
 
@@ -22,11 +22,12 @@ if __name__ == "__main__":
     data_type = get_data_type()
     DX1_index = int(data_type.index('DX1'))
     DX15_index = int(data_type.index('DX15'))
-    total_cases = 0
+    total_cases = [0, 0, 0, 0, 0]
     with open('raw_data/NEDS_2012_CORE.csv') as raw_file:
         reader = csv.reader(raw_file)
         for line in reader:
-            if TORSION_CODE in line[DX1_index:DX15_index]:
-                total_cases += 1
+            for t in range(len(TORSION_CODES)):
+                if TORSION_CODES[t] in line[DX1_index:DX15_index]:
+                    total_cases[t] += 1
 
     print total_cases
